20c526234500d106b38a8a79cef5f467409230a7
Shawn Petros <arashcuzi@gmail.com>
2016-02-26 11:56:35 -0600

    more working

--- app/controllers/FormController.js
+++ app/controllers/FormController.js
@@ -9,16 +9,6 @@ module.exports = function($scope, $http) {
         });
     };
 
-    $scope.test = function() {
-        console.log('worked');
-        data = {
-            data: 'some string total 44.21'
-        };
-        $.post('http://52.90.38.61:3004/jsonify', data).success(function(data){
-            console.log(data);
-        });
-    };
-
     $scope.processForm = function() {
         var file = $scope.file;
         console.log('file is ');
@@ -66,11 +56,11 @@ module.exports = function($scope, $http) {
 
         $.post('http://52.90.38.61:3004/jsonify', fd)
         .success(function(data) {
-            $scope.main.loading = false;
+            $scope.upload();
             console.log(data);
         })
         .error(function(err) {
-            $scope.main.error = true;
+            $scope.upload();
             $scope.main.loading = false;
             console.log(err);
         });
